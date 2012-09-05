49fcc82bbbc597092416af1befbe1a2bb7a1e257
Bart <belison@gmail.com>
2012-09-04 18:09:43 -0600

    first commit, rails shell

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheGrue::Application.config.secret_token = 'bf0d48eb700066126806051f2d2583caeca5f2ba83716d739264d809b493f4ca16c4f33a93144b43024ca81a79b4d26a948bbafb62284d156e88f88bb6d34627'
